9a68148e162c4bd4460d36789b57ed0bd10e6cb1
Squashim <konrad18v1@wp.pl>
2022-09-02 14:51:16 +0200

    Detail page finished

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import { renderDashboard } from "./view-dashboard.js";
 import { renderDetail } from "./view-detail.js";
 
+const themeBtn = document.querySelector("");
+
 if (window.location.search.includes("?country=")) {
 	document.querySelector(".filters").classList.add("hidden");
 	renderDetail();
